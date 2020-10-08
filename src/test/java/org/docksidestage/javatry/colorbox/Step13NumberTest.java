f1d640b23c19283cf0bb0a525ee3a4a31b344403
Marcin Matłacz <marcin.matlacz@bizreach.co.jp>
2020-10-08 07:53:57 +0200

    Add Step13NumberTest

--- src/test/java/org/docksidestage/javatry/colorbox/Step13NumberTest.java
+++ src/test/java/org/docksidestage/javatry/colorbox/Step13NumberTest.java
@@ -15,6 +15,16 @@
  */
 package org.docksidestage.javatry.colorbox;
 
+import java.math.BigDecimal;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+
+import org.docksidestage.bizfw.colorbox.ColorBox;
+import org.docksidestage.bizfw.colorbox.size.BoxSize;
+import org.docksidestage.bizfw.colorbox.space.BoxSpace;
+import org.docksidestage.bizfw.colorbox.yours.YourPrivateRoom;
 import org.docksidestage.unit.PlainTestCase;
 
 /**
@@ -33,6 +43,15 @@ public class Step13NumberTest extends PlainTestCase {
      * (カラーボックの中に入っているInteger型で、0から54までの値は何個ある？)
      */
     public void test_countZeroToFiftyFour_IntegerOnly() {
+        List<ColorBox> colorBoxes = new YourPrivateRoom().getColorBoxList();
+        long numValuesInRange = colorBoxes.stream()
+                .flatMap(colorBox -> colorBox.getSpaceList().stream())
+                .map(boxSpace -> boxSpace.getContent())
+                .filter(content -> content instanceof Integer)
+                .map(content -> (Integer) content)
+                .filter(value -> value >= 0 && value <= 54)
+                .count();
+        log(numValuesInRange);
     }
 
     /**
@@ -40,6 +59,17 @@ public class Step13NumberTest extends PlainTestCase {
      * (カラーボックの中に入っている数値で、0から54までの値は何個ある？)
      */
     public void test_countZeroToFiftyFour_Number() {
+        List<ColorBox> colorBoxes = new YourPrivateRoom().getColorBoxList();
+        BigDecimal low = new BigDecimal(0);
+        BigDecimal high = new BigDecimal(54);
+        long numValuesInRange = colorBoxes.stream()
+                .flatMap(colorBox -> colorBox.getSpaceList().stream())
+                .map(boxSpace -> boxSpace.getContent())
+                .filter(content -> content instanceof Number)
+                .map(value -> new BigDecimal(value.toString()))
+                .filter(value -> value.compareTo(low) > 0 && value.compareTo(high) < 0)
+                .count();
+        log(numValuesInRange);
     }
 
     /**
@@ -47,6 +77,13 @@ public class Step13NumberTest extends PlainTestCase {
      * (カラーボックスの中で、Integer型の Content を持っていてBoxSizeの幅が一番大きいカラーボックスの色は？)
      */
     public void test_findColorBigWidthHasInteger() {
+        List<ColorBox> colorBoxes = new YourPrivateRoom().getColorBoxList();
+        Optional<String> colorName = colorBoxes.stream()
+                .filter(colorBox -> colorBox.getSpaceList().stream().anyMatch(boxSpace -> boxSpace.getContent() instanceof Integer))
+                .max(Comparator.comparingInt(b -> b.getSize().getWidth()))
+                .map(ColorBox::getColor)
+                .map(boxColor -> boxColor.getColorName());
+        log(colorName.orElse(""));
     }
 
     /**
@@ -54,6 +91,17 @@ public class Step13NumberTest extends PlainTestCase {
      * (カラーボックスの中に入ってる List の中の BigDecimal を全て足し合わせると？)
      */
     public void test_sumBigDecimalInList() {
+        List<ColorBox> colorBoxes = new YourPrivateRoom().getColorBoxList();
+        Optional<BigDecimal> sum = colorBoxes.stream()
+                .flatMap(colorBox -> colorBox.getSpaceList().stream())
+                .map(BoxSpace::getContent)
+                .filter(content -> content instanceof List)
+                .flatMap(content -> ((List<?>) content).stream())
+                .filter(content -> content instanceof BigDecimal)
+                .map(content -> new BigDecimal(content.toString()))
+                .reduce((a, b) -> a.add(b));
+
+        log(sum.orElse(new BigDecimal(0)));
     }
 
     // ===================================================================================
@@ -64,6 +112,15 @@ public class Step13NumberTest extends PlainTestCase {
      * (カラーボックスに入ってる、valueが数値のみの Map の中で一番大きいvalueのkeyは？)
      */
     public void test_findMaxMapNumberValue() {
+        List<ColorBox> colorBoxes = new YourPrivateRoom().getColorBoxList();
+        Optional<? extends Map.Entry<?, ?>> entry = colorBoxes.stream()
+                .flatMap(colorBox -> colorBox.getSpaceList().stream())
+                .map(BoxSpace::getContent)
+                .filter(content -> content instanceof Map)
+                .filter(content -> ((Map<?, ?>) content).values().stream().allMatch(value -> value instanceof Number))
+                .flatMap(map -> ((Map<?, ?>) map).entrySet().stream())
+                .max(Comparator.comparing(e -> new BigDecimal(e.getValue().toString())));
+        log(entry);
     }
 
     /**
@@ -71,5 +128,21 @@ public class Step13NumberTest extends PlainTestCase {
      * (purpleのカラーボックスに入ってる Map の中のvalueの数値・数字の合計は？)
      */
     public void test_sumMapNumberValue() {
+        List<ColorBox> colorBoxes = new YourPrivateRoom().getColorBoxList();
+        Optional<BigDecimal> objects = colorBoxes.stream()
+                .filter(colorBox -> colorBox.getColor().getColorName().equals("purple"))
+                .flatMap(colorBox -> colorBox.getSpaceList().stream())
+                .map(BoxSpace::getContent)
+                .filter(content -> content instanceof Map)
+                .flatMap(content -> ((Map<?, ?>) content).values().stream())
+                .map(value -> {
+                    try {
+                        return new BigDecimal(value.toString());
+                    } catch (NumberFormatException e) {
+                        return new BigDecimal(0);
+                    }
+                })
+                .reduce((n1, n2) -> n1.add(n2));
+        log(objects);
     }
 }
